d5f6dd3f935e468b0899c3be289b47ca032d9d25
Elif Tuğul <140808923+eliftugull@users.noreply.github.com>
2023-11-20 04:18:05 +0300

    Add files via upload

---
+++ cinema reservation/degiskenler.js
@@ -0,0 +1,176 @@
+//console.log("degiskenler.js dosyası bağlandı")
+
+//=*=*= JavaScript veri türü
+
+/*
+string= 
+
+*/
+
+// //string ifade örneği
+// document.write("string bir değerim ben");
+// document.write("<br>");
+// //number yani saısal değer örneği
+// document.write;
+
+// //=*=*= JavaScript Değiskenler=*=*=//
+
+// /*
+// JavaScript degisken tanımlanırken kullanılan anahtar kelimeler
+// var==> artık kullanılmıyor
+// let
+// const
+
+// degisken tanımlarken camelCase yazım metodu
+//  ilk kelimenin ilk harfi küçün ikinci kelimenin
+//  ilk harfi büyük yazılır.
+
+// var degiskeninAdi=degiskeninDegeri
+
+// ilk kelime anahtar kelime
+// eşişttirin sol tarafı degiskenin aldıgı isim
+// esittirin sag tarafı degiskenin aldıgı deger
+
+// */
+
+// document.write("merhaba");
+// document.write("<br>");
+
+// var dogumTarihi;
+// document.write(dogumTarihi);
+// document.write("<br>");
+// dogumTarihi = 1998;
+// document.write(dogumTarihi);
+// document.write("<br>");
+
+// var adiniz = "elif";
+// var soyadiniz = "tugul";
+// document.write(adiniz);
+// document.write("<br>");
+// document.write(soyadiniz);
+
+// //farklı birden fazla değişkeni birleştirmek içim + operatörü kullanılır
+
+// var ogrencininAdi = "Elif";
+// var ogrencininSoyadi = "Tugul";
+// document.write("<br>");
+// //birden fazla değişkeni birleştirme
+// document.write(ogrencininAdi + "  " + ogrencininSoyadi);
+// document.write("<br>");
+// document.write(ogrencininSoyadi);
+
+//aaaaaa
+
+// var egitmeninYasi = 25;
+// var egitmeninAdi = "Hasan";
+// document.write('<br>')
+// document.write('<br>')
+// document.write('<br>')
+// document.write('<br>')
+// document.write('<br>')
+// document.write('<br>')
+// document.write('<br>')
+// var personelAdi = "Kemal";
+// document.write(personelAdi);
+// document.write("<br>");
+// var personelAdi = 53;
+// document.write(personelAdi);
+
+//let anahtar kelimesi ile değişken tanımlama
+//aynı ikinci bir değişken tanımlanamaz.
+//lt personelAdi='Kemal'
+//document.write(personelAdi)
+//document.write('<br>')
+//let personelAdi=53
+
+// let kullaniciAdi = prompt("adınızı Girin");
+// let kullaniciMemleket = prompt("Memleket Girin");
+// document.write(kullaniciAdi);
+// document.write("<br>");
+// document.write(kullaniciMemleket);
+// document.write("<br>");
+// document.write(
+//   "Kullanıcı Adı: " + kullaniciAdi  +  "kullanıcı memleket:" + kullaniciMemleket
+// );
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+//====matematiksel operatörler===//
+
+let sayi1 = 17;
+let sayi2 = 5;
+//toplama işlemi
+
+let toplam = sayi1 + sayi2;
+document.write("sayı 1 ve sayı 2 nin toplamı", toplam);
+document.write("<br>");
+document.write("<br>");
+
+//çıkarma işlemi
+
+let fark = sayi1 - sayi2;
+document.write("sayı 1 ve sayı 2 nin farkı", fark);
+
+document.write("<br>");
+document.write("<br>");
+
+//çarpma işlemi
+
+let carpim = sayi1 * sayi2;
+document.write("sayı 1 ve sayı 2 nin çarpımı", carpim);
+
+document.write("<br>");
+document.write("<br>");
+
+//bölme işlemi
+
+let bolme = sayi1 / sayi2;
+document.write("sayı 1 ve sayı 2 nin bölüm", bolme);
+
+document.write("<br>");
+document.write("<br>");
+
+//mod alma işlemi(bir sayının diğerine bölümünden kalan)
+let mod = sayi1 % sayi2;
+document.write("sayı 1 ve sayı 2 nin mod", mod);
+document.write("<br>");
+document.write("<br>");
+
+let sayac = 0;
+document.write("sayacın güncel değeri:", sayac);
+document.write("<br>");
+document.write("<br>");
+sayac = sayac + 3;
+document.write("sayacın güncel değeri:", sayac);
+document.write("<br>");
+document.write("<br>");
+
+sayac++;
+document.write("sayacın güncel değeri:", sayac);
+document.write("<br>");
+document.write("<br>");
+
+sayac--;
+document.write("sayacın güncel değri:", sayac);
+document.write("<br>");
+document.write("<br>");
+
+let sayi = 10;
+document.write("sayının değeri:", sayi);
+document.write("<br>");
+document.write("<br>");
+
+sayi %= 4;
+document.write("sayının değeri:", sayi);
+document.write("<br>");
+document.write("<br>");
+
+let birinciSayi = 10;
+let ikinciSayi = 4;
+ mods = birinciSayi % ikinciSayi;
+document.write('birincisayi ikinci sayı modu');
+document.write('<br>')
+document.write('<br>')
